acc8b112641b0be138dd2b74568471f4a4e6a333
amerigroup <andywj2ee@gmail.com>
2017-01-17 16:31:52 +0800

    second change Producer IP

--- src/kafka/examples/Producer.java
+++ src/kafka/examples/Producer.java
@@ -50,15 +50,13 @@ public class Producer extends Thread {
 		String[] hosts = { "www.taobao.com" };
 		String[] session_id = { "ABYH6Y4V4SCVXTG6DPB4VH9U123", "XXYH6YCGFJYERTT834R52FDXV9U34", "BBYH61456FGHHJ7JL89RG5VV9UYU7",
 				"CYYH6Y2345GHI899OFG4V9U567", "VVVYH6Y4V4SFXZ56JIPDPB4V678" };
-		String[] time = { "2017-01-07 08:40:50", "2017-01-07 08:40:51", "2017-01-07 08:40:52", "2017-01-07 08:40:53", 
-				"2017-01-07 09:40:49", "2017-01-07 10:40:49", "2017-01-07 11:40:49", "2017-01-07 12:40:49" };
+		String[] time = { "2016-01-07 08:40:50", "2016-01-07 08:40:51", "2016-01-07 08:40:52", "2016-01-07 08:40:53", 
+				"2016-01-07 09:40:49", "2016-01-07 10:40:49", "2016-01-07 11:40:49", "2016-01-07 12:40:49" };
 		String[] province_id = { "1","2","3","4","5","6" };
 		
 		
 		for (int i = 0; i < 500; i++) {			
 			producer.send(new KeyedMessage<Integer, String>(topic, hosts[0]+"\t"+session_id[random.nextInt(5)]+"\t"+time[random.nextInt(8)]+"\t"+province_id[random.nextInt(5)]));
-		   
-			System.out.println("produce msg i="+i);
 		}
 
 	}
